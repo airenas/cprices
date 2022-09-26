51074650d4c5dd88473b6ea67f5cfa91e5d707d2
Airenas Vaičiūnas <airenass@gmail.com>
2022-09-26 15:02:37 +0300

    Update sql schema

--- sql/000001_tables.down.sql
+++ sql/000001_tables.down.sql
@@ -2,9 +2,6 @@
 --drops tables
 BEGIN;
 
-DROP TABLE IF EXISTS "eth_prices";
-DROP TABLE IF EXISTS "btc_prices";
 DROP TABLE IF EXISTS "crypto_prices";
-DROP TABLE IF EXISTS "currency_info";
 
 COMMIT;
--- sql/000001_tables.up.sql
+++ sql/000001_tables.up.sql
@@ -1,28 +1,8 @@
 
 --schema.sql
 --SQL statements for a database schema cryptocurrency analysis
---Timescale Inc.
---Author: Avthar Sewrathan
 
 BEGIN;
---Schema for currency_info table
-CREATE TABLE "currency_info"(
-    currency_code	VARCHAR (10),
-    currency 		TEXT
-);
-
---Schema for btc_prices table
-CREATE TABLE "btc_prices"(
-    time            TIMESTAMP WITH TIME ZONE NOT NULL,
-    opening_price   DOUBLE PRECISION,
-    highest_price   DOUBLE PRECISION,
-    lowest_price    DOUBLE PRECISION,
-    closing_price   DOUBLE PRECISION,
-    volume_btc      DOUBLE PRECISION,
-    volume_currency DOUBLE PRECISION,
-    currency_code   VARCHAR (10)
-);
-
 --Schema for crypto_prices table
 CREATE TABLE "crypto_prices"(
     time            TIMESTAMP WITH TIME ZONE NOT NULL,
@@ -32,24 +12,11 @@ CREATE TABLE "crypto_prices"(
     closing_price   DOUBLE PRECISION,
     volume_crypto   DOUBLE PRECISION,
     volume_btc      DOUBLE PRECISION,
-    currency_code   VARCHAR (10)
-);
-
---Schema for eth_prices table
-CREATE TABLE "eth_prices"(
-    time            TIMESTAMP WITH TIME ZONE NOT NULL,
-    opening_price   DOUBLE PRECISION,
-    highest_price   DOUBLE PRECISION,
-    lowest_price    DOUBLE PRECISION,
-    closing_price   DOUBLE PRECISION,
-    volume_eth      DOUBLE PRECISION,
-    volume_currency DOUBLE PRECISION,
-    currency_code   VARCHAR (10)
+    currency_pair   VARCHAR (10),
+    PRIMARY KEY (time, currency_pair)
 );
 
 --Timescale specific statements to create hypertables for better performance
-SELECT create_hypertable('btc_prices', 'time', 'opening_price', 2);
-SELECT create_hypertable('eth_prices', 'time', 'opening_price', 2);
-SELECT create_hypertable('crypto_prices', 'time', 'currency_code', 2);
+SELECT create_hypertable('crypto_prices', 'time', 'currency_pair', 2);
 
 COMMIT;
